d5ced0666db72ce42d45fa8d29630e8ec3f2913c
Sneha Singh <snehasingh@Snehas-MacBook-Air.local>
2022-02-04 22:05:34 -0800

    add tests for size formatter

--- public/js/Utils/__tests__/commonUtils.test.js
+++ public/js/Utils/__tests__/commonUtils.test.js
@@ -1,8 +1,13 @@
 import { formatSize } from '../commonUtils';
 
-describe ("size format is converted properly", () => {
-    test("number of bytes are converted to the correct readable format", () => {
-        expect(formatSize(39000)).toBe('38.1 KB');
+describe ("size format is converted properly to the correct readable format", () => {
+    test("number of bytes are converted to KB", () => {
         expect(formatSize(1648876)).toBe('1.6 MB');
+    })
+    test("number of bytes are converted to MB", () => {
+        expect(formatSize(1648876)).toBe('1.6 MB');
+    })
+    test("number of bytes are converted to GB", () => {
+        expect(formatSize(2073741824)).toBe('1.9 GB');
     })
 });
