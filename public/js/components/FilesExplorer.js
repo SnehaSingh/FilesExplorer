00ea0fffd96859c60f183c553a52f639f7676544
Sneha Singh <snehasingh@Snehas-MacBook-Air.local>
2022-02-05 21:06:37 -0800

    File Explorer update and unite test

--- public/js/components/FilesExplorer.js
+++ public/js/components/FilesExplorer.js
@@ -8,10 +8,11 @@ import {
 } from '../Utils/sortUtils.js';
 import { formatSize } from '../Utils/commonUtils.js';
 
-function FilesExplorer(folderList) {
-    let currentFiles = [];
+function FilesExplorer() {
+    let currentTreeNodes = [];
     let sortOrder = '';
 
+    // clear out the table before the update operation
     const clearTable = () => {
         const collection = document.querySelectorAll('tr');
         for (let i = 0; i < collection.length; i++) {
@@ -21,66 +22,71 @@ function FilesExplorer(folderList) {
         }
     }
 
-    const updateExplorer = (filesList) => {
+    // given a tree node, this function creates a table row
+    const createExplorerRow = (treeNode) => {
+        const { type, name, modified, size } = treeNode;
+        const tr = document.createElement('tr');
+        tr.className = "table__data";
+
+        const nameElement = document.createElement('td');
+        nameElement.className = "explorer__nameColumn";
+        const dateElement = document.createElement('td');
+        dateElement.className = "explorer__dateColumn";
+        const sizeElement = document.createElement('td');
+        sizeElement.className = "explorer__sizeColumn";
+
+        if (type === "folder") {
+            let nameButton = document.createElement("button");
+            nameButton.className = "explorer__buttons";
+            nameButton.innerHTML = `<i class="fa fa-folder" aria-hidden="true"></i> ${name}`;
+            nameButton.addEventListener("click", () => {
+                updateExplorer(treeNode?.children);
+            });
+            nameElement.appendChild(nameButton);
+        }
+        else if (type === "file") {
+            let iconNode = document.createElement("span");
+            iconNode.className = "fa fa-file";
+            iconNode.style.paddingRight = "3px";
+            const nameTextNode = document.createTextNode(name);
+            nameElement.appendChild(iconNode);
+            nameElement.appendChild(nameTextNode);
+        }
+
+        const dateTextNode = document.createTextNode(modified);
+        const formattedSize = formatSize(size);
+        const sizeTextNode = document.createTextNode(formattedSize);
+
+        dateElement.appendChild(dateTextNode);
+        sizeElement.appendChild(sizeTextNode);
+        tr.appendChild(nameElement);
+        tr.appendChild(dateElement);
+        tr.appendChild(sizeElement);
+        return tr;
+    }
+
+    // function attaches rows to the table
+    const updateExplorer = (treeNodesList) => {
         const table = document.getElementById("explorerTable");
 
         // clear the explorer table before updating it
         clearTable();
-        currentFiles = [];
-        for (let i = 0; i < filesList.length; i++){
-            const { type, name, modified, size } = filesList[i];
-            currentFiles.push(filesList[i]);
-            const tr = document.createElement('tr');
-            tr.className = "table__data";
-        
-            const nameElement = document.createElement('td');
-            nameElement.className = "explorer__nameColumn";
-            const dateElement = document.createElement('td');
-            dateElement.className = "explorer__dateColumn";
-            const sizeElement = document.createElement('td');
-            sizeElement.className = "explorer__sizeColumn";
-
-            if (type === "folder") {
-                let nameButton = document.createElement("button");
-                nameButton.className = "explorer__buttons";
-                nameButton.innerHTML = `<i class="fa fa-folder" aria-hidden="true"></i> ${name}`;
-                nameButton.addEventListener("click", () => {
-                    updateExplorer(filesList[i]?.children);
-                });
-                nameElement.appendChild(nameButton);
-            }
-            else if (type === "file") {
-                let iconNode = document.createElement("span");
-                iconNode.className = "fa fa-file";
-                iconNode.style.paddingRight = "3px";
-                const nameTextNode = document.createTextNode(name);
-                nameElement.appendChild(iconNode);
-                nameElement.appendChild(nameTextNode);
-            }
-            
-            const dateTextNode = document.createTextNode(modified);
-            const formattedSize = formatSize(size);
-            const sizeTextNode = document.createTextNode(formattedSize);
-        
-            dateElement.appendChild(dateTextNode);
-            sizeElement.appendChild(sizeTextNode);
-            tr.appendChild(nameElement);
-            tr.appendChild(dateElement);
-            tr.appendChild(sizeElement);
+        currentTreeNodes = [];
+        for (let i = 0; i < treeNodesList.length; i++){
+            currentTreeNodes.push(treeNodesList[i]);
+            const tr = createExplorerRow(treeNodesList[i]);
             table.appendChild(tr);
         }
     };
 
     const sortByColumn = (desc, asc) => {
         if (sortOrder === "Ascending") {
-            currentFiles.sort(desc);
+            currentTreeNodes.sort(desc);
             sortOrder = "Descending";
         }
-        else {
-            currentFiles.sort(asc);
-            sortOrder = "Ascending";
-        }
-        updateExplorer(currentFiles);
+        currentTreeNodes.sort(asc);
+        sortOrder = "Ascending";
+        updateExplorer(currentTreeNodes);
     }
 
     const nameHeader = document.getElementById("name__header");
---
+++ public/js/components/__tests__/FilesExplorer.test.js
@@ -0,0 +1,61 @@
+/**
+ * @jest-environment jsdom
+ */
+import FilesExplorer from '../FilesExplorer';
+
+const mockTree = [
+    {
+      "id": "1",
+      "type": "folder",
+      "name": "Documents",
+      "modified": "12/12/2021",
+      "size": 1024,
+      "children": []
+    },
+    {
+      "id": "2",
+      "type": "file",
+      "name": "sample.txt",
+      "modified": "05/11/2019",
+      "size": 12340028,
+      "children": []
+    }
+]
+
+let table;
+
+describe("Tests for FileExplorer", () => {
+  beforeEach(() => {
+    table = document.createElement("table");
+    table.setAttribute("id", "explorerTable");
+    const column1 = document.createElement("th");
+    column1.setAttribute("id", "name__header");
+    const column2 = document.createElement("th");
+    column2.setAttribute("id", "date__header");
+    const column3 = document.createElement("th");
+    column3.setAttribute("id", "size__header");
+    table.appendChild(column1);
+    table.appendChild(column2);
+    table.appendChild(column3);
+    document.body.appendChild(table);
+  });
+
+  test("explorer shows all Tree Nodes", () => {
+    const { update } = FilesExplorer();
+    update(mockTree);
+    expect(table.querySelector(".explorer__nameColumn")).not.toBeNull();
+    expect(table.querySelector(".explorer__dateColumn")).not.toBeNull();
+    expect(table.querySelector(".explorer__sizeColumn")).not.toBeNull();
+
+    const firstRow = table.querySelectorAll("tr")[0];
+    expect(firstRow.childNodes[0].childNodes[0].textContent).toBe(" Documents");
+    expect(firstRow.childNodes[1].textContent).toBe("12/12/2021");
+    expect(firstRow.childNodes[2].textContent).toBe("1.0 KB");
+    const secondRow = table.querySelectorAll("tr")[1];
+    expect(secondRow.childNodes[0].textContent).toBe("sample.txt");
+    expect(secondRow.childNodes[1].textContent).toBe("05/11/2019");
+    expect(secondRow.childNodes[2].textContent).toBe("11.8 MB");
+    // const firstRow = table.querySelectorAll("tr")[0];
+    // expect(firstRow.firstChild.firstChild.textContent).toBe("Documents")
+  })
+})
