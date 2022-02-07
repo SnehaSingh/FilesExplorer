510b74656cfb5c9c0fa1d399f8af402774e18c3b
Sneha Singh <snehasingh@Snehas-MacBook-Air.local>
2022-02-06 22:29:52 -0800

    loader animation

--- public/app.js
+++ public/app.js
@@ -5,6 +5,8 @@ import getJsonData from "./js/Utils/getData.js";
 function createExplorer() {
     const { createTree } = Sidebar();
     const { update } = FilesExplorer();
+    let loading = true;
+    const loadingAnimation = document.querySelector(".loader");
 
     const updateExplorer = (nodesList) => {
         update(nodesList);
@@ -12,11 +14,14 @@ function createExplorer() {
 
     getJsonData("/files")
     .then(data => {
+        loading = false;
+        loadingAnimation.classList.remove("active");
         const sidebarNavComponent = createTree(data, updateExplorer);
         document.getElementById("navbar").appendChild(sidebarNavComponent);
     })
     .catch(error => {
         console.log("An error has occurred while fetching data. ", error);
+        loading = false;
     });
 }
 
